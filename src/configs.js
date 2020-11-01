8f5d107e61a42426c138468360e628bd775ee787
Matan Yechiel <matan@rele.ai>
2020-11-01 15:28:40 +0200

    update configs

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 export const cvLink =
-  "https://drive.google.com/file/d/1GD7D_aUjMEQxUXs_h2gErRkBnUGHAh-V/view?usp=sharing"
+  "https://drive.google.com/file/d/1n20LFuJUYSxI91lsm86uIoVdsWPRawhS/view?usp=sharing"
 export const githubLink = "https://github.com/matany90"
 export const facebookLink = "https://www.facebook.com/matan.yechiel"
 export const linkedinLink =
