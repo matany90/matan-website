ed44aa3e184d9937ae4a0d8923b029a323d8106c
Matan Yechiel <matan@rele.ai>
2021-05-01 11:23:42 +0300

    updated cv

--- src/configs.js
+++ src/configs.js
@@ -1,5 +1,5 @@
 export const cvLink =
-  "https://drive.google.com/file/d/1n20LFuJUYSxI91lsm86uIoVdsWPRawhS/view?usp=sharing"
+  "https://drive.google.com/file/d/1L8DsfEDRMGSQ0ls85k53aC330oKooJtb/view?usp=sharing"
 export const githubLink = "https://github.com/matany90"
 export const facebookLink = "https://www.facebook.com/matan.yechiel"
 export const linkedinLink =
